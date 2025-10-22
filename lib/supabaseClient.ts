0437e8acf8202c1b67a1ae1a8421a82dae3da15d
zabi <zabiullah.j.m@gmail.com>
2025-10-22 21:23:29 +0330

    4455

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Platform } from 'react-native';
+let AsyncStorage: any = null;
+if (typeof window === 'undefined' || Platform.OS !== 'web') {
+    AsyncStorage = require('@react-native-async-storage/async-storage').default;
+}
 
 const SUPABASE_URL = 'https://xcpzagvohhdnexmlgxjm.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhjcHphZ3ZvaGhkbmV4bWxneGptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwOTIxMzYsImV4cCI6MjA3NTY2ODEzNn0.Zi6UY_LcA_zqlKz5AXr8MDD2WazvlDMMp8P3tl0R1nk';
