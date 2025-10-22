b30d0ed3f2e87b0df570c3da3c124dc54749b2c2
zabi <zabiullah.j.m@gmail.com>
2025-10-22 22:13:42 +0330

    5566

--- scripts/ci-polyfills.js
+++ scripts/ci-polyfills.js
@@ -1,11 +1,56 @@
-// Polyfill for CI environment
+// Polyfill for CI environment - Complete version
 if (typeof window === 'undefined') {
-    global.window = {};
-    global.document = {};
-    global.navigator = {};
+    global.window = {
+        document: {
+            getElementsByTagName: () => [],
+            createElement: () => ({
+                setAttribute: () => { },
+                appendChild: () => { },
+            }),
+            head: {
+                appendChild: () => { },
+            },
+        },
+        navigator: {
+            userAgent: '',
+        },
+        location: {
+            href: '',
+            protocol: 'https:',
+            host: '',
+            hostname: '',
+            port: '',
+            pathname: '',
+            search: '',
+            hash: '',
+            origin: 'https://localhost',
+        },
+    };
+
+    global.document = global.window.document;
+    global.navigator = global.window.navigator;
+    global.location = global.window.location;
+
     global.localStorage = {
         getItem: () => null,
         setItem: () => { },
         removeItem: () => { },
+        clear: () => { },
+        key: () => null,
+        length: 0,
+    };
+
+    global.sessionStorage = {
+        getItem: () => null,
+        setItem: () => { },
+        removeItem: () => { },
+        clear: () => { },
+        key: () => null,
+        length: 0,
+    };
+
+    // Prevent vaul from trying to insert CSS
+    global.CSS = {
+        supports: () => false,
     };
 }
