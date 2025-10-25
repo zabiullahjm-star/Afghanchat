45c3e08c69f7c37b7d582b2e735db152430fa3da
zabi <zabiullah.j.m@gmail.com>
2025-10-25 13:56:05 +0330

    login3

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,10 +1,18 @@
 import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SUPABASE_URL = 'https://xcpzagvohhdnexmlgxjm.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhjcHphZ3ZvaGhkbmV4bWxneGptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwOTIxMzYsImV4cCI6MjA3NTY2ODEzNn0.Zi6UY_LcA_zqlKz5AXr8MDD2WazvlDMMp8P3tl0R1nk';
 
 // کاملاً پاک - بدون هیچ import اضافی
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+    auth: {
+        storage: AsyncStorage,
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: false,
+    }
+});
 
 // export مستقیم
 export const { auth, storage, from } = supabase;
